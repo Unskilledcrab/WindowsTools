17c4e8cf1d5882297bfeea14204751707a10fb24
Jeremy Buentello <jb09056@georgiasouthern.edu>
2020-09-13 22:47:12 -0400

    updating the filescanner more. Need to still impliment the service on the viewmodel side

--- src/FileCleanup/Services/FileScanner.cs
+++ src/FileCleanup/Services/FileScanner.cs
@@ -1,22 +1,61 @@
-﻿using FileCleanup.ProgressModels;
+﻿using FileCleanup.Models;
+using FileCleanup.ProgressModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using FileCleanup.Extensions;
+using System.Windows.Media;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace FileCleanup.Services
 {
-    class FileScanner
+    public class FileScanner
     {
-        public event EventHandler ScanComplete;
-        public event EventHandler DirectoryAdded;
-        public event EventHandler FileAdded;
+        #region Properties
+        public ObservableCollection<FileProps> FlaggedFiles { get; private set; } = new ObservableCollection<FileProps>();
+        public ObservableCollection<FileProps> FlaggedDirectories { get; private set; } = new ObservableCollection<FileProps>();
 
         private CancellationTokenSource token = new CancellationTokenSource();
         private Stopwatch stopwatch = new Stopwatch();
+        private Configuration Configuration;
+        #endregion
+
+        public event EventHandler ScanComplete;
+        public event EventHandler<FileProps> DirectoryAdded;
+        public event EventHandler<FileProps> FileAdded;
+        public event EventHandler<TimeSpan> ScannerCancelled;
+
+        #region Constructors
+        public FileScanner(long flagFileSize, DateTime flagLastAccessDate)
+        {
+            Configuration = new Configuration
+            {
+                FlagFileSize = flagFileSize,
+                LastAccessFlagDate = flagLastAccessDate
+            };
+        }
+
+        public FileScanner(Configuration configuration)
+        {
+            Configuration = configuration;
+        }
+        #endregion
+
+        public void UpdateConfiguration(Configuration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void CancelScan()
+        {
+            token.Cancel();
+        }
 
         public async Task StartScanner(IProgress<ScanProgress> progress)
         {
@@ -33,16 +72,104 @@ namespace FileCleanup.Services
                 catch (OperationCanceledException)
                 {
                     var timeElapsed = stopwatch.Elapsed;
-                    ScanningStatus = $"Cancelled {timeElapsed:c}";
+                    OnScannerCancelled(timeElapsed);
+                }
+                catch (Exception ex)
+                {
+
                 }
             }
-
             OnScanComplete(EventArgs.Empty);
         }
 
+        private async Task Scan(string path, IProgress<ScanProgress> progress, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+                token.ThrowIfCancellationRequested();
+
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                try
+                {
+                    await ScanDirectory(directory, progress, token);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private async Task ScanDirectory(string directory, IProgress<ScanProgress> progress, CancellationToken token)
+        {
+            progress.Report(new ScanProgress(directory));
+
+            if (DoNotScan(directory))
+                return;
+
+            var directoryToAdd = new DirectoryInfo(directory);
+            if (CanAddDirectory(directoryToAdd))
+                progress.Report(new ScanProgress(new FileProps(directoryToAdd), false));
+
+            foreach (var file in Directory.GetFiles(directory))
+            {
+                if (token.IsCancellationRequested)
+                    token.ThrowIfCancellationRequested();
+                ScanFile(file, progress);
+            }
+            await Scan(directory, progress, token);
+        }
+
+        private void ScanFile(string file, IProgress<ScanProgress> progress)
+        {
+            if (DoNotScan(file))
+                return;
+
+            var fileToAdd = new FileInfo(file);
+            if (CanAddFile(fileToAdd))
+                progress.Report(new ScanProgress(new FileProps(fileToAdd), true));
+        }
+
+        private bool CanAddDirectory(DirectoryInfo directory)
+        {
+            var accessTimeCheck = Configuration.IsOverFlagAccessDate(directory.LastAccessTime);
+
+            return (accessTimeCheck);
+        }
+
+        private bool CanAddFile(FileInfo fileInfo)
+        {
+            var lengthCheck = Configuration.IsOverFlagSize(fileInfo.Length);
+            var accessTimeCheck = Configuration.IsOverFlagAccessDate(fileInfo.LastAccessTime);
+
+            return (lengthCheck && accessTimeCheck);
+        }
+
+        private bool DoNotScan(string path)
+        {
+            return Configuration.PathsNotToScan.Contains(path);
+        }
+
+        #region Events
         protected virtual void OnScanComplete(EventArgs e)
         {
             ScanComplete?.Invoke(this, e);
         }
+
+        protected virtual void OnFileAdded(FileProps file)
+        {
+            FileAdded?.Invoke(this, file);
+        }
+
+        protected virtual void OnDirectoryAdded(FileProps directory)
+        {
+            DirectoryAdded?.Invoke(this, directory);
+        }
+
+        protected virtual void OnScannerCancelled(TimeSpan cancelTime)
+        {
+            ScannerCancelled?.Invoke(this, cancelTime);
+        }
+        #endregion
     }
 }
